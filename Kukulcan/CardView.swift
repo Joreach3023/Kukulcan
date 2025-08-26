3c9b7f5d7c7808fd869ba5b358fb69038505f336
Joreach3023 <157987099+Joreach3023@users.noreply.github.com>
2025-08-25 23:59:07 -0400

    Use MayaRiver image for card back

--- Kukulcan/CardView.swift
+++ Kukulcan/CardView.swift
@@ -8,7 +8,8 @@ struct CardBackView: View {
     var body: some View {
         let h = width * ratio
 
-        Image("LaunchLogo")
+        // Utilise l'image "MayaRiver" comme dos de carte
+        Image("MayaRiver")
             .resizable()
             .scaledToFill()
             .frame(width: width, height: h)
