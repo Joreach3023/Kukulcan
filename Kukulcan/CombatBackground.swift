2d3f13e7f049fcab6060e1c14f01d3129e208b95
Joreach3023 <157987099+Joreach3023@users.noreply.github.com>
2026-03-08 19:38:13 -0400

    Reduce combat background image scale

--- Kukulcan/CombatBackground.swift
+++ Kukulcan/CombatBackground.swift
@@ -2,17 +2,21 @@ import SwiftUI
 
 struct CombatBackground: View {
     var body: some View {
-        ZStack {
-            Image("bg_combat_jungle")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
+        GeometryReader { geo in
+            ZStack {
+                Color.black.ignoresSafeArea()
 
-            // Vignette pour contraste
-            LinearGradient(colors: [.black.opacity(0.35), .clear, .black.opacity(0.4)],
-                           startPoint: .top, endPoint: .bottom)
-            .ignoresSafeArea()
+                Image("bg_combat_jungle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width, height: geo.size.height)
+                    .ignoresSafeArea()
+
+                // Vignette pour contraste
+                LinearGradient(colors: [.black.opacity(0.35), .clear, .black.opacity(0.4)],
+                               startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            }
         }
     }
 }
-
